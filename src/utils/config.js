66767557843e8d5e27e54de3d53f4baaa0f232ff
wave~ <3725612+ybning@users.noreply.github.com>
2019-07-20 00:13:29 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteName: '物模块配置',
   copyright: '物模块配置',
   logoPath: '/logo.svg',
-  //apiPrefix: process.env.NODE_ENV === 'production'?'http://www.mlcloud.com.cn/thingapi':'http://192.168.1.102:8062',
-  apiPrefix: 'http://www.mlcloud.com.cn/thingapi',
+  //apiPrefix: process.env.NODE_ENV === 'production'?'http://www.xxx.com.cn/thingapi':'http://192.168.1.102:8062',
+  apiPrefix: 'http://www.xxx.com.cn/thingapi',
   fixedHeader: true, // sticky primary layout header
 
   /* Layout configuration, specify which layout to use for route. */
